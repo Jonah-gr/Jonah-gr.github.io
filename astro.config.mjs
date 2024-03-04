bcec122fca4865c2d99ce56177d81913c51d14d7
Jonah Gräfe <jonah.greafe@gmail.com>
2024-03-04 23:44:57 +0100

    integrated preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://Jonah-gr.github.io"
+  site: "https://Jonah-gr.github.io",
+  integrations: [preact()]
 });
