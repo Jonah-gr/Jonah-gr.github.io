fb119adc0c1d1fcf3d684b5371881ba1611ab2a4
Jonah Gräfe <jonah.greafe@gmail.com>
2024-03-05 05:24:02 +0100

    added icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://Jonah-gr.github.io",
-  integrations: [preact()]
+  integrations: [preact(), icon()]
 });
